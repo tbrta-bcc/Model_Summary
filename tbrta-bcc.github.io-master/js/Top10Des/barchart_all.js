e8e6349f0eb5d0d2c87b7c1c23476d2f9decac72
BCC <69207749+tbrta-bcc@users.noreply.github.com>
2022-08-09 15:03:15 -0400

    upload files

---
+++ tbrta-bcc.github.io-master/js/Top10Des/barchart_all.js
@@ -0,0 +1,51 @@
+//Travel Flow Bar Chart	
+
+// Initialize the chart	
+
+		var ctx = document.getElementById("myChart").getContext("2d");
+
+		var myBarChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ["Top1", "Top2", "Top3", "Top4", "Top5", "Top6","Top7","Top8","Top9","Top10"],
+				datasets: [{
+					label: barchartLabel,
+					data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					backgroundColor: [
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)',
+						commonColor+'0.2)'
+					],
+					borderColor: [
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)',
+						commonColor+'1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
+			
